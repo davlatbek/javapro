a6a0d7b42bd109c19b2fc74c74561fcc409f8d18
Davlat Isroilov <vtb4074088@region.vtb.ru>
2024-03-11 17:51:03 +0300

    hw3

---
+++ src/ru/innotech/javapro/hw3/MyExecutorService.java
@@ -0,0 +1,54 @@
+package ru.innotech.javapro.hw3;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class MyExecutorService {
+    private final LinkedList<Runnable> taskList;
+    private final List<Thread> threadList;
+    private boolean isRunning = true;
+
+    public MyExecutorService(int numberOfWorkers) {
+        this.threadList = new ArrayList<>(numberOfWorkers);
+        this.taskList = new LinkedList<>();
+        for (int i = 0; i < numberOfWorkers; i++) {
+            Thread thread = new Thread(() -> {
+                while (isRunning) {
+                    Runnable runnable;
+                    synchronized (taskList) {
+                        while (taskList.isEmpty() && isRunning) {
+                            try {
+                                taskList.wait();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        if (!taskList.isEmpty()) {
+                            runnable = taskList.removeFirst();
+                        } else {
+                            continue;
+                        }
+                    }
+                    runnable.run();
+                }
+            });
+            threadList.add(thread);
+            thread.start();
+        }
+    }
+
+    public void execute(Runnable task) {
+        synchronized (taskList) {
+            if (isRunning) {
+                taskList.addLast(task);
+                taskList.notify();
+            }
+        }
+    }
+
+    public void shutdown() {
+        this.isRunning = false;
+        this.threadList.forEach(t -> t.interrupt());
+    }
+}
